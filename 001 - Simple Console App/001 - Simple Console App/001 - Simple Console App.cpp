4494d2694cf8aaddf99fe29672480f33b10bf8d2
Scott Edwards <scotteza@gmail.com>
2019-12-10 11:57:38 +0000

    Pass a reference to a Person object into a function

--- 001 - Simple Console App/001 - Simple Console App/001 - Simple Console App.cpp
+++ 001 - Simple Console App/001 - Simple Console App/001 - Simple Console App.cpp
@@ -18,7 +18,7 @@ void PutAPersonOnTheHeap()
     p->Name = "Scott";
 }
 
-void ProcessPerson(Person p)
+void ProcessPersonAsACopy(Person p)
 {
     cout << "Passed-in person's name: ";
     cout << p.Name << endl;
@@ -27,7 +27,7 @@ void ProcessPerson(Person p)
 
     p.Name = "Name 2";
 
-    cout << "Printing changed person name" << endl;
+    cout << "Changed person name: ";
     cout << p.Name << endl;
 }
 
@@ -42,22 +42,66 @@ void PassACopyOfAPersonToAFunction()
     cout << "Person's name pre-method call: ";
     cout << p.Name << endl;
 
-    // This will create a copy of the person and send that to the function "ProcessPerson"
-    // i.e. it will NOT pass a reference to the object "p" to the function "ProcessPerson"
-    ProcessPerson(p);
+    // This will create a copy of the person and send that to the function "ProcessPersonAsACopy"
+    // i.e. it will NOT pass a reference to the object "p" to the function "ProcessPersonAsACopy"
+    ProcessPersonAsACopy(p);
 
     cout << "Person's name post-method call: ";
     cout << p.Name << endl;
+}
 
-    cout << endl;
-    system("PAUSE");
+void ProcessAPersonAsAReference(Person* p)
+{
+    cout << "Passed-in person's name: ";
+    cout << p->Name << endl;
+
+    cout << "Changing person's name..." << endl;
+
+    p->Name = "Name 2";
+
+    cout << "Changed person name: ";
+    cout << p->Name << endl;
+}
+
+void PassPersonReferenceToAFunction()
+{
+    cout << "Creating person..." << endl;
+    Person p;
+
+    cout << "Setting person's name..." << endl;
+    p.Name = "Name 1";
+
+    cout << "Person's name pre-method call: ";
+    cout << p.Name << endl;
+
+    // This will pass a reference to the created person to a function
+    ProcessAPersonAsAReference(&p);
+
+    cout << "Person's name post-method call: ";
+    cout << p.Name << endl;
 }
 
 int main()
 {
-    /*PutAPersonOnTheStack();
-    PutAPersonOnTheHeap(); */
+    /*
+     *PutAPersonOnTheStack();
+    PutAPersonOnTheHeap();
+    */
+
+    cout << "----------------------------------------" << endl;
+    cout << "Passing copies of objects" << endl;
+    cout << "----------------------------------------" << endl;
     PassACopyOfAPersonToAFunction();
+
+    cout << endl << endl;
+
+    cout << "----------------------------------------" << endl;
+    cout << "Passing object references" << endl;
+    cout << "----------------------------------------" << endl;
+    PassPersonReferenceToAFunction();
+
+    cout << endl << endl;
+    system("PAUSE");
 
     return 0;
 }
