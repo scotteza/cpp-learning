a5d955767f5bc91d4f4c4435aa02c331ebf59b71
Scott Edwards <scotteza@gmail.com>
2019-12-10 11:29:55 +0000

    Create a Person on the stack and a Person on the heap

--- 001 - Simple Console App/001 - Simple Console App/001 - Simple Console App.cpp
+++ 001 - Simple Console App/001 - Simple Console App/001 - Simple Console App.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
+#include "Person.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // This calls the default constructor
+    // It adds this instance to the stack (not the heap)
+    Person p1;
+
+    // This puts the instance created onto the heap
+    // The star tells us that this is a pointer to the address of the object
+    Person *p2 = new Person;
 
     return 0;
 }
