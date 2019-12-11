842846475bcd329077e066957c8661412e0e868b
Scott Edwards <scotteza@gmail.com>
2019-12-11 17:49:31 +0000

    Learn about dynamic linking

--- 003 - Compilation And Linking/003 - Compilation And Linking/main.cpp
+++ 003 - Compilation And Linking/003 - Compilation And Linking/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include "../StaticLib/Mathematics.h"
+#include "../MathLibrary/MathLibrary.h"
 
 using namespace std;
 
@@ -16,13 +17,25 @@ void Foo()
 
 #define MUL(a,b) a*b
 
+void CallAnExternalLibrary()
+{
+    // Initialize a Fibonacci relation sequence.
+    fibonacci_init(1, 1);
+    // Write out the sequence values until overflow.
+    do {
+        cout << fibonacci_index() << ": " << fibonacci_current() << endl;
+    } while (fibonacci_next());
+    // Report count of values written before overflow.
+    cout << fibonacci_index() + 1 << " Fibonacci sequence values fit in an " << "unsigned 64-bit integer." << endl;
+}
+
 int main()
 {
     assert(MUL(2, 4) == 8);
 
     int x = 1;
     int y = Add1(x);
-    
+
     // This will fail as it substitutes values in to the macro
     // So, MUL(2, 3 + 1) will get substituted for 2 * 3 + 1
     // assert(MUL(2, 3 + 1) == 8);
@@ -32,6 +45,8 @@ int main()
     Foo();
 
     cout << "20 + 22 = " << Add(20, 22);
+
+    CallAnExternalLibrary();
 
     getchar();
 
