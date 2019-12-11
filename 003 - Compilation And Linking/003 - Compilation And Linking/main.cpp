4bf94291efe8dd5d5f0858fa05daceb54194610e
Scott Edwards <scotteza@gmail.com>
2019-12-11 15:24:07 +0000

    Learn about macros

--- 003 - Compilation And Linking/003 - Compilation And Linking/main.cpp
+++ 003 - Compilation And Linking/003 - Compilation And Linking/main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <cassert>
 using namespace std;
+
+#if _DEBUG
+void Foo()
+{
+    cout << "We are in debug mode!" << endl;
+}
+#endif
+
+#define MUL(a,b) a*b
 
 int main()
 {
+    assert(MUL(2, 4) == 8);
+
+    // This will fail as it substitutes values in to the macro
+    // So, MUL(2, 3 + 1) will get substituted for 2 * 3 + 1
+    // assert(MUL(2, 3 + 1) == 8);
+
     cout << "Hello, C++" << endl;
 
-#if _DEBUG
-    cout << "We are in debug mode!" << endl;
-#endif
+    Foo();
 
     getchar();
 
