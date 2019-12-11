34969327ca569fd265c62fa4a536cc2f079a263b
Scott Edwards <scotteza@gmail.com>
2019-12-11 18:18:37 +0000

    Use the DllImport attribute to call C++ code from C#

---
+++ 003 - Compilation And Linking/CSharpConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSharpConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 * 3 = {0}", Multiply(2, 3));
+            Console.ReadKey();
+        }
+
+        [DllImport("MathLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Multiply(int a, int b);
+    }
+}
