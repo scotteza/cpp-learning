1167f2439f7df3f879ca07f9c9596c2afe1c011b
Scott Edwards <scotteza@gmail.com>
2019-12-12 14:23:57 +0000

    Learn about namespaces and global scope

--- 004 - Functions And Variables/004 - Functions And Variables/004 - Functions And Variables.cpp
+++ 004 - Functions And Variables/004 - Functions And Variables/004 - Functions And Variables.cpp
@@ -1,7 +1,32 @@
 #include "pch.h"
 using namespace std;
 
+namespace life
+{
+    auto Meaning = 42;
+    auto* PointerToMeaning = &Meaning;
+    auto& ReferenceToMeaning = Meaning;
+}
+
 int main()
 {
+    using life::Meaning;
+
     cout << "Hello World!" << endl;
+
+    cout << Meaning << " takes up " << sizeof(Meaning) << " bytes." << endl;
+
+    cout << "Changing the value of Meaning using a pointer..." << endl;
+    *life::PointerToMeaning = 43;
+    cout << "Meaning is now equal to " << Meaning << endl;
+
+    cout << "Changing the value of Meaning using a reference..." << endl;
+    life::ReferenceToMeaning = 44;
+    cout << "Meaning is now equal to " << Meaning << endl;
+
+    cout << endl << "Press return to exit." << endl;
+
+    getchar();
+
+    return 0;
 }
