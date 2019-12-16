aaec662a91e8db00d7de56c0dd8c1f7a5d41b30d
Scott Edwards <scotteza@gmail.com>
2019-12-16 13:50:19 +0000

    Learn about objects deleting themselves

--- 006 - Memory Management/006 - Memory Management/006 - Memory Management.cpp
+++ 006 - Memory Management/006 - Memory Management/006 - Memory Management.cpp
@@ -37,6 +37,33 @@ public:
     }
 };
 
+class Person2
+{
+private:
+    // Now anything outside the class cannot hit these constructors and destructors
+    Person2() {}
+    Person2(const Person2& _) {}
+    ~Person2() {}
+    int referenceCount;
+
+public:
+    // Public factory method that returns a pointer to a person
+    static Person2* Create()
+    {
+        auto* p = new Person2;
+        p->referenceCount = 1;
+        return p;
+    }
+    void AddRef() { referenceCount++; }
+    void Release()
+    {
+        if (--referenceCount == 0)
+        {
+            delete this;
+        }
+    }
+};
+
 int main()
 {
     Address* a;
