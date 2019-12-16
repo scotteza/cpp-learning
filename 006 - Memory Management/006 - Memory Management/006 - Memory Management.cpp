0fbaf438a229c1c2bcc5072cc5efd59f3078971c
Scott Edwards <scotteza@gmail.com>
2019-12-16 14:06:56 +0000

    Learn about l-value, r-value and move semantics

--- 006 - Memory Management/006 - Memory Management/006 - Memory Management.cpp
+++ 006 - Memory Management/006 - Memory Management/006 - Memory Management.cpp
@@ -7,6 +7,11 @@ class Address
 public:
     string city;
 
+    Address()
+    {
+
+    }
+
     Address(const string& city)
         : city(city)
     {
@@ -63,6 +68,59 @@ public:
         }
     }
 };
+
+
+
+
+
+
+
+
+
+int MeaningOfLife()
+{
+    return 42;
+}
+
+void increment(int& x)
+{
+    x++;
+}
+
+// An "l-value" can appear on the left-hand side of an assignment statement
+// An "r-value" can appear on the right-hand side of an assignment statement
+void moveRValue()
+{
+    int a = 0;
+    increment(a);
+
+    // Compiler will moan here
+    // increment(1);
+
+    // This also doesn't make sense, as the meaning of life is an rvalue
+    // You cannot go MeaningOfLife() = 12;
+    // int& x = MeaningOfLife();
+
+    // This is an r-value reference and is valid
+    int&& y = MeaningOfLife();
+    cout << y << endl;
+
+    Address book[100];
+    // This will create an Address using the constructor
+    // Then it will copy it, and put the new object at book[55]
+    book[55] = Address("Home");
+
+    // You could also have an array of pointers:
+    Address* book2[100];
+    book2[55] = new Address("Home");
+
+    // But, what if you want an array of references?
+    // This won't work:
+    // Address& book3[100];
+    // But we can use move semantics to initialise without wasting memory
+    book[56] = move(Address("Work"));
+
+}
 
 int main()
 {
